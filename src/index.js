03acd477c051eec9ee13365bd9054d2b9f685a3e
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-07-10 19:30:06 -0300

    creating navbar

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Nosotros from "./screen/Nosotros";
+import Contacto from "./screen/Contacto";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Nosotros",
+    element: <Nosotros />,
+  },
+  {
+    path: "/Contacto",
+    element: <Contacto />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
