c9c7c32e004a1a7553d7b9ebac8e22cbad0702d9
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-07-18 12:55:37 -0300

    componente reutilizable

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+function NavbarReutilizable() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>
+              {" "}
+              <Link to={`/`} style={{ textDecoration: "none", color: "black" }}>
+                Home
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link
+                to={`/Nosotros`}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Nosotros
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              {" "}
+              <Link
+                to={`/Contacto`}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Contacto
+              </Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarReutilizable;
--- src/screen/Nosotros.js
+++ src/screen/Nosotros.js
@@ -1,48 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 function Nosotros() {
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">Logo</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link>
-                {" "}
-                <Link
-                  to={`/`}
-                  style={{ textDecoration: "none", color: "black" }}
-                >
-                  Home
-                </Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link
-                  to={`/Nosotros`}
-                  style={{ textDecoration: "none", color: "black" }}
-                >
-                  Nosotros
-                </Link>
-              </Nav.Link>
-              <Nav.Link>
-                {" "}
-                <Link
-                  to={`/Contacto`}
-                  style={{ textDecoration: "none", color: "black" }}
-                >
-                  Contacto
-                </Link>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Navbar />
       <h2>Nosotros</h2>
     </div>
   );
