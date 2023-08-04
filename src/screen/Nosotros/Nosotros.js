f583ec941f34d3c27f34ad4edc2340107d8bc42a
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-08-04 13:20:36 -0300

    fix

--- src/screen/Nosotros/Nosotros.js
+++ src/screen/Nosotros/Nosotros.js
@@ -73,15 +73,6 @@ function Nosotros() {
         </Card.Body>
       </Card>
       <br />
-      {/* <Card>
-        <Card.Body>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-        <Card.Img variant="bottom" src="holder.js/100px180" />
-      </Card> */}
       <Footer />
     </div>
   );
